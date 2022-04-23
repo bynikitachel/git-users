c22e5e930f00e41a2fd85f1389833a0688263dbd
bynikitachel <nikitavyatiorec@gmail.com>
2022-04-23 12:37:29 +0300

    added component navbar

---
+++ src/components/NavBar/index.js
@@ -0,0 +1,12 @@
+import './index.scss';
+
+const NavBar = () => {
+    return (
+        <div className='navbar'>
+            <div className='git-icon'></div>
+            <input placeholder='Enter GitHub username' />
+        </div>
+    )
+}
+
+export default NavBar;
