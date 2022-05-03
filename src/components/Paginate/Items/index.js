9ad4261e3c774667eee940bedbfdd9a41c415a84
bynikitachel <nikitavyatiorec@gmail.com>
2022-05-03 12:01:13 +0300

    fix link rel

--- src/components/Paginate/Items/index.js
+++ src/components/Paginate/Items/index.js
@@ -1,11 +1,10 @@
 const Items = ({ currentItems }) => {
-    // console.log(currentItems);
     return (
         <>
             {currentItems &&
                 currentItems.map((item, i) => (
                     <div key={i} className='container-rep'>
-                        <div className='rep-name'><a href={item.clone_url} target="_blank">{item.name}</a></div>
+                        <div className='rep-name'><a href={item.clone_url} target="_blank" rel="noreferrer">{item.name}</a></div>
                         {item.description && <div className='rep-description'>{item.description}</div>}
                     </div>
                 ))}
